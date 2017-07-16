1888b012af7de4e48ccf3ecfd9095f381b9b5243
Levi Donaldson <lcdbacanas@gmail.com>
2017-07-15 20:35:23 -0500

    Update counter.elm

--- counter.elm
+++ counter.elm
@@ -1,1 +1,34 @@
+import Html exposing (beginnerProgram, div, button, text)
+import Html.Events exposing (onClick)
 
+
+main =
+  beginnerProgram { model = 0, view = view, update = update }
+
+
+view model =
+  div []
+    [ button [ onClick Increment ] [ text "+" ]
+    , div [] [ text (toString model) ]
+    , button [ onClick Decrement ] [ text "-" ]
+    , div []
+      [
+        button [onClick Clear] [ text "Clear"] 
+      ]
+    ]
+    
+
+
+type Msg = Increment | Decrement | Clear
+
+
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    Decrement ->
+      model - 1
+
+    Clear ->
+      model * 0
